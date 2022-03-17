1eaa516ca4f0c8d71cf53e2e1ed750303de7a71b
lykapro <95994547+lykapro@users.noreply.github.com>
2022-03-17 13:14:13 +0800

    Update maxmenu.php

--- maxmenu.php
+++ maxmenu.php
@@ -1,7 +1,7 @@
 <?PHP
 $Webs ="LYKAPro Dev Team\n\n";
 $copyright ="© 2022 Rene Aparri\n";
-$Script = "MAXmenu build [02.28.2022]\n\n";
+$Script = "MAXmenu build [03.17.2022]\n\n";
 
 @system("clear");
 
